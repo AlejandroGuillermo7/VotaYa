b23c2cc936aaf74f89a59029d0d26094ee5fe523
AlejandroGuillermo7 <ale1111prepa7@gmail.com>
2026-07-29 12:01:00 -0600

    cambio prueba

--- votaya-frontend/src/pages/Login.jsx
+++ votaya-frontend/src/pages/Login.jsx
@@ -382,12 +382,7 @@ function Login({
             </div>
           </div>
 
-          <p
-            className="loginContraseña"
-            onClick={irARecuperar}
-          >
-            ¿Olvidaste tu contraseña?
-          </p>
+          
 
           {error && (
             <p className="loginError">
